6b46585d48699e202bcf030ee0d9d623b2a1dec0
EMichira <emmanuelmichira@gmail.com>
2017-07-14 12:03:05 +0300

    Configured model hook in index.js to import data from firebase

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,7 @@
 import Ember from 'ember';
 
-var message = [{
-  id: 1,
-  question: "Who is the latest Arsenal signing this summer?",
-  author: "Anuelmich",
-  info: "Hint: He played in Ligue1"
-},{
-  id: 2,
-  question: "Who's Arsenal longest serving manager?",
-  author: "Anuelmich",
-  info: "Hint: The incredibles. Unbeaten"
-}, {
-  id: 3,
-  question: "Who is the best player of the season 2016/2017 for Arsenal?",
-  author: "Anuelmich",
-  info: "Hint: Dogs"
-}];
-
 export default Ember.Route.extend({
   model() {
-    return message;
+    return this.store.findAll('message');
   },
 });
