538561470bb38b68f1fe91c7507fb6b26aa6ca95
EMichira <emmanuelmichira@gmail.com>
2017-07-14 15:47:03 +0300

    Updated new-message.js to add functionality to the button

--- app/components/new-message.js
+++ app/components/new-message.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewMessage: false,
+  action: {
+    messageFormShow() {
+      this.set('addNewMessage', true);
+    }
+  }
 });
