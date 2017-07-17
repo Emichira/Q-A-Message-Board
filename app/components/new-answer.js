3cab445576a470fe674b4973dbfe0f45ae54990f
EMichira <emmanuelmichira@gmail.com>
2017-07-17 16:47:22 +0300

    Updated code for new-answer

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    saveAnswer1() {
+      var params = {
+        jibu: this.get('jibu'),
+        mtunzi: this.get('mtunzi'),
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('saveAnswer2', params);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-      model() {
-        return this.store.findAll('message');
-          },
+  model() {
+    return this.store.findAll('message');
+  },
 
-          actions: {
-            saveMessage3(params) {
-              var newMessage = this.store.createRecord('message', params);
-              newMessage.save();
-              this.transitionTo('index');
-            },
-          }
-        });
+  actions: {
+    saveMessage3(params) {
+      var newMessage = this.store.createRecord('message', params);
+      newMessage.save();
+      this.transitionTo('index');
+    },
+    saveAnswer3(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
+      this.transitionTo('index');
+    },
+  }
+});
