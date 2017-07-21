16218b5048f3cf3bd0bf798c5d0b992cb1c588bb
EMichira <emmanuelmichira@gmail.com>
2017-07-21 13:09:14 +0300

    Added logic to popularity helper to check number of answers

--- app/helpers/message-popularity.js
+++ app/helpers/message-popularity.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export function messagePopularity(params/*, hash*/) {
-  return params;
+  var message = params[0];
+
+  if(message.get('answers').get('length') >= 5 {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"><span>');
+  }
 }
 
 export default Ember.Helper.helper(messagePopularity);
