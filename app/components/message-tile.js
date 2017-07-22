8b3e70626ef4f4a29c5f3a1feb984166eadcf032
EMichira <emmanuelmichira@gmail.com>
2017-07-22 15:23:48 +0300

    Added logic code to file display answer count

--- app/components/message-tile.js
+++ app/components/message-tile.js
@@ -3,4 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   updateMessageForm: false,
 
+  answerCount: Ember.computed('message.answers', function() {
+    var answerAmount = this.get('message.answers.length');
+    return answerAmount;
+  })
 });
